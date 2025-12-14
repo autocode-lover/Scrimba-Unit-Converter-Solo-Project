408d2caa604804da2bed6e88e65122413cdf4f43
Vasanth <cvasanth73@gmail.com>
2025-12-14 13:11:38 +0530

    Added Volumes and Mass Conversions

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ let inputEl = document.getElementById("input-el")
 let userEnteredValue =""
 
 
-let lengthConversionsPara = document.getElementById("length-convertion-values")
+let lengthConversionsPara = document.getElementById("length-conversion-values")
+
+let volumesConversionsPara = document.getElementById ("volumes-conversion-values")
+
+let massConversionsPara = document.getElementById ("mass-conversion-values")
 
 
 btnEl.addEventListener("click", function (){
@@ -13,7 +17,10 @@ btnEl.addEventListener("click", function (){
 console.log(userEnteredValue)
 
 renderLength(userEnteredValue)
+renderVolumes(userEnteredValue)
+renderMass(userEnteredValue)
 })
+
 
 function renderLength(anyValue) {
     const metersToFeet = anyValue * 3.28084
@@ -25,6 +32,30 @@ function renderLength(anyValue) {
     lengthConversionsPara.innerHTML = `${anyValue} meters to feet is ${roundFeetValue} ft and ${anyValue} feet to meters is ${roundMetersValue} mts`
 
     inputEl.value=""
+}
+
+function renderVolumes(anyValue){
+    const literstoGallons = anyValue* 0.264172
+    const gallonstoLiters = anyValue/0.264172
+
+    let roundGallonsValue= literstoGallons.toFixed(2)
+    let roundLitersValue = gallonstoLiters.toFixed(2)
+
+    volumesConversionsPara.innerHTML = `${anyValue} liters = ${roundGallonsValue} gallons | ${anyValue} gallons = ${roundLitersValue} liters` 
+}
+
+function renderMass(anyValue){
+    const kilogramsToPounds = anyValue* 2.20462
+    const poundsToKilograms = anyValue/ 2.20462
+
+    let roundPoundsValue = kilogramsToPounds.toFixed(2)
+    let roundKilogramsValue = 
+    poundsToKilograms.toFixed(2)
+
+
+    massConversionsPara.innerHTML=
+    `${anyValue} Kilograms = ${roundPoundsValue} Pounds | ${anyValue} Pounds = ${roundKilogramsValue} Kilograms` 
+
 }
 
 
